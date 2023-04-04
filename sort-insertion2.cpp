d10ccb4aea8f0d82ba1edaecfcfca94ef7bf01c2
Readiz <readiz@readiz.com>
2023-04-04 17:48:19 +0900

    Update sort-insertion2.cpp

--- sort-insertion2.cpp
+++ sort-insertion2.cpp
@@ -2,8 +2,8 @@ void sort(int data[], int size) {
     register int i;
     register int j;
     register int k;
-    // a b c d e f x
-    // a ~ f는 정렬된 상태, x를 어디에 삽입할지 본다.
+    // a b c d e f k
+    // a ~ f는 정렬된 상태, k를 어디에 삽입할지 본다.
     for(i = 1; i < size; ++i) {
         k = data[i];
         // d[j-1] < d[j] 여야하는데 뒤집혀있으면 swap
