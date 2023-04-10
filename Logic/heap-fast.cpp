2ca79d79b25afc2c080ca8ba7ac64ce55fce5d8c
Readiz <readiz@readiz.com>
2023-04-10 08:15:06 +0000

    Deploying to gh-pages from @ Readiz/CodingNote@f0d491801d3e4152db387488ef28f102000f1c52 🚀

--- Logic/heap-fast.cpp
+++ Logic/heap-fast.cpp
@@ -7,11 +7,9 @@ int sz = 0;
 void insert(int val) {
     // Heapify Up
     register int i = ++sz;
-    register int p;
     for(; i > 1; i >>= 1) {
-        p = i >> 1;
-        if (val >= data[p]) break;
-        data[i] = data[p];
+        if (val >= data[i>>1]) break;
+        data[i] = data[i>>1];
     }
     data[i] = val;
 }
@@ -26,10 +24,10 @@ int getMin() {
     // 적어도 왼쪽 자식을 포함하고 &&
     // 오른쪽 자식도 포함하면서 && 오른쪽이 더 조건에 부합하면 c | 1을 사용한다.
     // 그리고 자식이 최종적으로 부모보다 작으면, swap
-    for(; c <= sz && data[c |= (c < sz && data[c | 1] < data[c])] < val; c <<= 1) {
-        data[c >> 1] = data[c];
+    for(; c <= sz && data[c |= (c < sz && data[c|1] < data[c])] < val; c <<= 1) {
+        data[c>>1] = data[c];
     }
-    data[c >> 1] = val;
+    data[c>>1] = val;
 
     return r;
 }
