78df999a97b62f4085026aa0a614f5a26630ddc9
Readiz <readiz@readiz.com>
2023-04-26 07:50:39 +0000

    Deploying to gh-pages from @ Readiz/CodingNote@8cd70be4cd9aa3db6d24e7e41d6729ddee0e97d2 🚀

--- Logic/sieve-legacy.cpp
+++ Logic/sieve-legacy.cpp
@@ -7,10 +7,17 @@ std::vector<int> getPrimes(int MAX) {
     // 1 ~ MAX까지의 소수의 수를 구한다.
     // 결과는 prime 배열에 저장됨
 
+    for(register int i = 2; i * i <= MAX; ++i) {
+        if (isNotPrime[i] == 0) {
+            for(register int j = i * i; j <= MAX; j += i) {
+                isNotPrime[j] = 1;
+            }
+        }
+    }
+
     for(register int i = 2; i <= MAX; ++i) {
-        if (isNotPrime[i] == 0) primes.push_back(i);
-        for(register int j = 2; j * i <= MAX; ++j) {
-            isNotPrime[i * j] = 1;
+        if (isNotPrime[i] == 0) {
+            primes.push_back(i);
         }
     }
 
